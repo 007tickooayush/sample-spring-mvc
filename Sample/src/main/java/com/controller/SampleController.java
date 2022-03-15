591600a3b43296cd8e8a94ad28ea7b7a02b78ad6
007tickooayush <ayush.tickoo292000@gmail.com>
2022-03-15 14:45:36 +0530

    created template

---
+++ Sample/src/main/java/com/controller/SampleController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SampleController {
+	String message = "Welcome to Spring MVC!";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	
+	@RequestMapping("/")
+	public ModelAndView indexPage() {
+//		redirecting to index page
+		return new ModelAndView("index"); 
+	}
+}
